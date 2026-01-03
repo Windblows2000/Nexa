80286b5e3d063c90efb6ff93dfb897b835846598
Windblows2000 <y4m441400152@rcjy.edu.sa>
2026-01-03 10:40:47 +0300

    Update project branding and license headers

--- src/mpris/mod.rs
+++ src/mpris/mod.rs
@@ -1,7 +1,7 @@
 // Copyright (C) 2025 Windblows2000
-// This file is part of rusty-player.
+// This file is part of nexa.
 //
-// rusty-player is free software: you can redistribute it and/or modify
+// nexa is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
